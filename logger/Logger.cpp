7ec270501c3d9f00a87cba89eaea23a655452a72
nothingness02 <2847119841@qq.com>
2025-05-22 14:02:06 +0800

    添加线程安全队列，增加缓存机制

--- logger/Logger.cpp
+++ logger/Logger.cpp
@@ -1,6 +1,5 @@
 #include "Logger.h"
-
-using namespace yazi::utility;
+using namespace LOG::utility;
 #include <time.h>
 #include <stdexcept>
 #include <iostream>
@@ -18,10 +17,16 @@ Logger * Logger::m_log=nullptr;
 
 Logger::Logger():m_level(DEBUG),m_len(0),m_max(0)
 {
-
+    m_worker=std::thread(&Logger::work,this);//启动后台程序
 }
 Logger::~Logger(){
-    close();
+    {
+        std::lock_guard<std::mutex> lock(m_mutex);
+        m_stop=true;
+    }
+    m_cond.notify_all();
+    m_worker.join();//等待后台程序完成输入
+    close();   
 }
 Logger* Logger::instance(){
     if(m_log==nullptr)
@@ -61,32 +66,30 @@ void Logger::log(Level level,const char* file,int line,const char* format,...)
 
     const char* fmt="%s %s %s:%d ";
     int size=snprintf(NULL,0,fmt,timestamp,s_level[level],file,line);
+    char* buffer =new char[size+1];
     if(size>0){
-        char* buffer =new char[size+1];
         snprintf(buffer,size+1,fmt,timestamp,s_level[level],file,line);
         buffer[size]=0;
-        m_fout<<buffer;
         m_len+=size;
-        delete[] buffer;
     }
     va_list arg_ptr;
     va_start(arg_ptr,format);
     size=vsnprintf(NULL,0,format,arg_ptr);
     va_end(arg_ptr);
+    char * content =new char[size+1];
     if(size>0){
-        char * content =new char[size+1];
         va_start(arg_ptr,format);
         vsnprintf(content,size+1,format,arg_ptr);
         va_end(arg_ptr);
-        m_fout<<content;
         m_len+=size;
-        delete[] content;
     }
-    m_fout<<"\n";
-    m_fout.flush();
-    if(m_len>=m_max && m_max>0){ 
-        rotate();
+    {
+        std::lock_guard<std::mutex> lock(m_mutex);
+        m_queue.push(string(buffer)+string(content));
+        m_cond.notify_one();
     }
+    delete[] buffer;
+    delete[] content;
 }
 
 void Logger::rotate(){
@@ -101,4 +104,19 @@ void Logger::rotate(){
         throw std::logic_error("rename log file failed");
     }
     open(m_filename);
+}
+
+void Logger::work(){
+    while(true){
+        string content;
+        {
+            std::unique_lock<std::mutex> lock(m_mutex);
+            m_cond.wait(lock,[this]{return !m_queue.empty()|| !m_stop;});
+            if(m_queue.empty() && m_stop){break;}
+            content=m_queue.front();
+            m_queue.pop();
+        }
+        m_fout << content << "\n";
+        m_fout.flush();
+    }
 }
